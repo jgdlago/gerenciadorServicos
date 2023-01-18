f03fb0b11f47c0ea323dae5e7417c1d7b6d0e561
João Dal Lago <joaogabrieldallago@hotmail.com>
2023-01-18 10:35:22 -0300

    Add ClienteController

--- clientes/src/main/java/io/github/jgdlago/clientes/ClientesApplication.java
+++ clientes/src/main/java/io/github/jgdlago/clientes/ClientesApplication.java
@@ -1,7 +1,12 @@
 package io.github.jgdlago.clientes;
 
+import io.github.jgdlago.clientes.model.entity.Cliente;
+import io.github.jgdlago.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
@@ -9,4 +14,5 @@ public class ClientesApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientesApplication.class, args);
     }
+
 }
