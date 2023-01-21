7f8b693bce002d226337488bafe33bf90c342c79
João Dal Lago <joaogabrieldallago@hotmail.com>
2023-01-21 02:51:03 -0300

    error handling and registration validations

---
+++ clientes/src/main/java/io/github/jgdlago/clientes/rest/ApplicationControllerAdvice.java
@@ -0,0 +1,27 @@
+package io.github.jgdlago.clientes.rest;
+
+import io.github.jgdlago.clientes.rest.exception.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+import java.util.List;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationErrors(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> messages = bindingResult.getAllErrors()
+                .stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(messages);
+    }
+}
